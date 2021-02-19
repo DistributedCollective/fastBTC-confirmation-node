b22319c1765b7626aa812a8ee29ea4d41ad29c8d
jamiemadrox779 <jamie.madrox779@gmail.com>
2021-02-19 13:56:29 +0000

    Part 1: Confirm withdraw request

--- controller/main.js
+++ controller/main.js
@@ -20,12 +20,43 @@ class MainController {
     }
 
     getWithdrawRequest(){
-
+        //1. parse all events
+        //2. for every event: call isConfirmed on the multisig to check wheter this proposal is still valid
+        //3. if so: confirmWithdrawRequest
     }
 
-    confirmWithdrawRequest(){
 
+    /*
+    /// @dev Allows an owner to confirm a transaction.
+	/// @param transactionId Transaction ID.
+	function confirmTransaction(uint256 transactionId)
+		public
+		ownerExists(msg.sender)
+		transactionExists(transactionId)
+		notConfirmed(transactionId, msg.sender)
+	{
+		confirmations[transactionId][msg.sender] = true;
+		emit Confirmation(msg.sender, transactionId);
+		executeTransaction(transactionId);
+	}
+
+    */
+    confirmWithdrawRequest(txId){
+        const wallet = await this.getWallet();
+        if (wallet.length == 0) return { error: "no wallet available to process the assignment" };
+        const nonce = await this.web3.eth.getTransactionCount(wallet, 'pending');
+        const gasPrice = await this.getGasPrice();
+        const data = this.prepareTransferFromMultisig(val, to);
+        const receipt = await this.multisig.methods.confirmTransaction(txId).send({
+            from: wallet,
+            gas: 100000,
+            gasPrice: gasPrice,
+            nonce: nonce
+        });
+        walletManager.decreasePending(wallet);
+        return receipt;
     }
+
 
 }
 
