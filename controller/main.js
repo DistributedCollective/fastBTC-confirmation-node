e42ea1b1f6f7145e70c92b205e5085f6905e2e96
jamiemadrox779 <jamie.madrox779@gmail.com>
2021-02-20 12:41:30 +0000

    Add mutex to package.json, add wallet controller, add confirmTx

--- controller/main.js
+++ controller/main.js
@@ -6,20 +6,23 @@ import multisigAbi from '../config/multisigabi';
 import contractAbi from "../config/contractAbi";
 import conf from '../config/config';
 import wallets from '../secrets/accounts';
-
+import { Mutex } from 'async-mutex';
+import walletManager from './walletCtrl';
 
 
 class MainController {
     constructor() {
         this.web3 = new Web3(conf.nodeProvider);
-        this.contractSovryn = new this.web3.eth.Contract(abiComplete, conf.sovrynProtocolAdr);
+        this.multisig = new this.web3.eth.Contract(abiComplete, conf.sovrynProtocolAdr);
+        this.mutex = new Mutex();
+        walletManager.init(this.web3);
     }
 
-    start(){
-        setInterval(this.getNewWithdrawRequest, 1000*60);
+    start() {
+        setInterval(this.getNewWithdrawRequest, 1000 * 60);
     }
 
-    getWithdrawRequest(){
+    getWithdrawRequest() {
         //1. parse all events
         //2. for every event: call isConfirmed on the multisig to check wheter this proposal is still valid
         //3. if so: confirmWithdrawRequest
@@ -28,31 +31,33 @@ class MainController {
 
     /*
     /// @dev Allows an owner to confirm a transaction.
-	/// @param transactionId Transaction ID.
-	function confirmTransaction(uint256 transactionId)
-		public
-		ownerExists(msg.sender)
-		transactionExists(transactionId)
-		notConfirmed(transactionId, msg.sender)
-	{
-		confirmations[transactionId][msg.sender] = true;
-		emit Confirmation(msg.sender, transactionId);
-		executeTransaction(transactionId);
-	}
+    /// @param transactionId Transaction ID.
+    function confirmTransaction(uint256 transactionId)
+        public
+        ownerExists(msg.sender)
+        transactionExists(transactionId)
+        notConfirmed(transactionId, msg.sender)
+    {
+        confirmations[transactionId][msg.sender] = true;
+        emit Confirmation(msg.sender, transactionId);
+        executeTransaction(transactionId);
+    }
 
     */
-    confirmWithdrawRequest(txId){
+    confirmWithdrawRequest(txId) {
+        console.log("confirm tx "+txId);
         const wallet = await this.getWallet();
         if (wallet.length == 0) return { error: "no wallet available to process the assignment" };
         const nonce = await this.web3.eth.getTransactionCount(wallet, 'pending');
         const gasPrice = await this.getGasPrice();
-        const data = this.prepareTransferFromMultisig(val, to);
+
         const receipt = await this.multisig.methods.confirmTransaction(txId).send({
             from: wallet,
-            gas: 100000,
+            gas: 1000000,
             gasPrice: gasPrice,
             nonce: nonce
         });
+
         walletManager.decreasePending(wallet);
         return receipt;
     }
---
+++ controller/walletCtrl.js
@@ -0,0 +1,54 @@
+/**
+ * Manages the wallets.
+ */
+import conf from '../config/config';
+import U from '../utils/helper';
+
+class WalletManager {
+    /**
+     * initiates the wallet list and imports the private key
+     * @param {*} web3 the web3 instance used by zhe rskCtrl
+     */
+    init(web3){
+        this.wallet={};
+        web3.eth.accounts.wallet.add(conf.account.pKey);
+        this.wallet = {
+            address: conf.account.adr,
+            pending: 0
+        };
+    }
+    
+    /**
+     * returns a wallet with less than 4 pending transactions
+     * @param {*} timeout the maximum waiting time  in ms
+     */
+    async getFreeWallet(timeout){
+        const stopAt = Date.now() + timeout;
+        while (Date.now() < stopAt){
+            if(this.wallet.pending < 4){
+                this.wallet.pending++;
+                return this.wallet.address;
+            }
+            await U.wasteTime(5);
+        }
+        return "";
+    }
+
+    
+    /**
+     * decreases the pending tx count for a wallet
+     * @param {*} wallet 
+     */
+    decreasePending(walletAddress){
+        if(this.wallet.address == walletAddress){
+            this.wallet.pending--;
+            return true;
+        }
+        
+        console.error("could not decrease the pending tx count for non-existing wallet address: "+walletAddress);
+        return false;
+    }
+
+}
+
+export default new WalletManager();
