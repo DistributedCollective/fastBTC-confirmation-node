61c3f0984e89239116291a3daaa35397dbb7b52f
jamiemadrox779 <jamie.madrox779@gmail.com>
2021-02-21 18:57:25 +0000

    Add missing methods

--- controller/main.js
+++ controller/main.js
@@ -6,7 +6,7 @@ import multisigAbi from '../config/multisigAbi';
 import conf from '../config/config';
 import { Mutex } from 'async-mutex';
 import walletManager from './walletCtrl';
-
+import U from '../utils/helper';
 
 class MainController {
     constructor() {
@@ -31,6 +31,7 @@ class MainController {
     async getWithdrawRequest(txId) {
         const tx = await this.multisig.methods["transactions"](txId).call();
         console.log(tx);
+        this.confirmWithdrawRequest(0);
     }
 
 
@@ -52,6 +53,32 @@ class MainController {
         return receipt;
     }
 
+     /**
+     * @notice loads a free wallet from the wallet manager 
+     * @dev this is secured by a mutex to make sure we're never exceeding 4 pending transactions per wallet
+     */
+    async getWallet() {
+        await this.mutex.acquire();
+        let wallet = "";
+        let timeout = 5 * 60 * 1000;
+        try {
+            //if I have to wait, any other thread needs to wait as well
+            wallet = await walletManager.getFreeWallet(timeout);
+            //because the node can't handle too many simultaneous requests
+            await U.wasteTime(0.5);
+            this.mutex.release();
+        }
+        catch (e) {
+            this.mutex.release();
+            console.error(e);
+        }
+        return wallet;
+    }
+
+    async getGasPrice() {
+        const gasPrice = await this.web3.eth.getGasPrice();
+        return Math.round(gasPrice);
+    }
 
 }
 
