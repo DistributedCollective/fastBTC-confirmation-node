b03e99c3850c9e68b41b36e48ae0348e0ad27432
Bitcoinera <anagjordano@gmail.com>
2021-02-21 19:56:12 +0100

    add getAllEvents function

--- controller/main.js
+++ controller/main.js
@@ -4,6 +4,7 @@
 import Web3 from 'web3';
 import multisigAbi from '../config/multisigAbi';
 import conf from '../config/config';
+import U from '../utils/helper';
 import { Mutex } from 'async-mutex';
 import walletManager from './walletCtrl';
 
@@ -17,8 +18,9 @@ class MainController {
     }
 
     start() {
+        this.getAllPastEvents()
         //setInterval(this.getNewWithdrawRequest, 1000 * 60);
-        this.getWithdrawRequest("0xc0ba8fe4b176c1714197d43b9cc6bcf797a4a7461c5fe8d0ef6e184ae7601e51");
+        // this.getWithdrawRequest("0xc0ba8fe4b176c1714197d43b9cc6bcf797a4a7461c5fe8d0ef6e184ae7601e51");
     }
 
     /*
@@ -28,6 +30,28 @@ class MainController {
         //2. for tx-id: call isConfirmed on the multisig to check wheter this proposal is still unconfirmed
         //3. if so: confirmWithdrawRequest
     */
+    async getAllPastEvents() {
+        const receipts = [];
+        const wallet = await this.getWallet();
+        const gasPrice = await this.getGasPrice();
+        const nonce = await this.web3.eth.getTransactionCount(wallet, 'pending');
+        const numberOfTransactions = await this.multisig.methods["getTransactionCount"](true, false).call();
+        const allTransactionsIds = await this.multisig.methods["getTransactionIds"](0, numberOfTransactions, true, false).call();
+        allTransactionsIds.forEach(async txId => {
+            const isConfirmed = await this.multisig.methods["isConfirmed"](txId).call();
+            if (!isConfirmed) {
+                const receipt = await this.multisig.methods.confirmTransaction(txId).send({
+                    from: wallet,
+                    gas: 1000000,
+                    gasPrice: gasPrice,
+                    nonce: nonce
+                });
+                receipts.push(receipt)
+            }
+        })
+        return receipts
+    }
+
     async getWithdrawRequest(txId) {
         const tx = await this.multisig.methods["transactions"](txId).call();
         console.log(tx);
@@ -52,7 +76,32 @@ class MainController {
         return receipt;
     }
 
+    /**
+     * @notice loads a free wallet from the wallet manager 
+     * @dev this is secured by a mutex to make sure we're never exceeding 4 pending transactions per wallet
+     */
+    async getWallet() {
+        await this.mutex.acquire();
+        let wallet = "";
+        let timeout = 5 * 60 * 1000;
+        try {
+            //if I have to wait, any other thread needs to wait as well
+            wallet = await walletManager.getFreeWallet(timeout);
+            //because the node can't handle too many simultaneous requests
+            await U.wasteTime(0.5);
+            this.mutex.release();
+        }
+        catch (e) {
+            this.mutex.release();
+            console.error(e);
+        }
+        return wallet;
+    }
 
+    async getGasPrice() {
+        const gasPrice = await this.web3.eth.getGasPrice();
+        return Math.round(gasPrice);
+    }
 }
 
 
