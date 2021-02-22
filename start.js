5798f6287b429b71272057fd757857bea00a1a8b
Bitcoinera <anagjordano@gmail.com>
2021-02-22 19:17:21 +0100

    add master salve logic: get nodes connected

--- start.js
+++ start.js
@@ -5,7 +5,18 @@ import conf from './config/config';
 const express= require('express');
 const app = express();
 const http = require('http').createServer(app);
+const io = require("socket.io-client"); 
 import MainCtrl from './controller/main';
+
+const socket = io("http://127.0.0.1:3007", {
+  reconnectionDelayMax: 10000,
+//   auth: {
+//     token: "123"
+//   },
+//   query: {
+//     "my-key": "my-value"
+//   }
+});
 
 console.log("Hola. Starting confirmation node on "+conf.network);
        
