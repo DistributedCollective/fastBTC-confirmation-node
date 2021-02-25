a7633eb2f4071ca3876bd4fb0332276c495ded79
jamiemadrox779 <jamie.madrox779@gmail.com>
2021-02-25 14:54:08 +0000

    Add auto account creation

---
+++ utils/createAccount.js
@@ -0,0 +1,40 @@
+/**
+ * Utility functions to prepare wallets
+ */
+
+import Web3 from 'web3';
+import conf from '../config/config';
+var fs = require('fs');
+var web3 = new Web3(conf.nodeProvider);
+
+const pass = process.argv[2];
+
+function createAccount() {
+    var account = web3.eth.accounts.create();
+    const ks = web3.eth.accounts.encrypt(account.privateKey, pass);
+
+    const w = {
+        main: {
+            adr: account.address.toLowerCase(),
+            ks: ks
+        }
+    }
+    const obj = "export default " + JSON.stringify(w);
+
+    fs.writeFile('./secrets/accounts.js', obj, (err)=> {
+        if (err) return console.log(err);
+        console.log("keystore created. Share this wallet address with your co-signers_ "+account.address.toLowerCase());
+    });
+}
+
+
+
+
+
+
+function decryptAccount() {
+    let r = web3.eth.accounts.decrypt(ks, p);
+    console.log(r);
+}
+
+createAccount();
