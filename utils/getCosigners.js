856cec3bdf458ba51fd0e6b08cadb622c10e827e
Bitcoinera <anagjordano@gmail.com>
2021-03-02 20:19:42 +0100

    working

--- utils/getCosigners.js
+++ utils/getCosigners.js
@@ -1,5 +1,4 @@
 import rskCtrl from '../controller/rskCtrl';
-import conf from '../config/config';
 
 const from ={
     adr: "",
@@ -11,25 +10,13 @@ export default async function getCosigners() {
     console.log("Getting Cosigners.\nInitializing RSK");
     await rskCtrl.init();
 
-    const pKey = conf.account.pKey || rskCtrl.web3.eth.accounts.decrypt(conf.account.ks, process.argv[3]).privateKey;
-    rskCtrl.web3.eth.accounts.wallet.add(pKey);
-
-    const data = rskCtrl.web3.eth.abi.encodeFunctionCall({
-        name: 'getOwners',
-        type: 'function',
-        inputs: []
-    }, []);
-
     try {
-        const cosigners = await rskCtrl.multisig.methods.submitTransaction(conf.multisigAddress, 0, data).send({
-            from: conf.account.adr,
-            gas: 100000
-        });
+        const cosigners = await rskCtrl.multisig.methods["getOwners"]().call()
     
         console.log("\nCosigners are", cosigners);
         return cosigners;
     } catch (e) {
-        console.log("\nError getting cosigners")
+        console.log("\nError getting cosigners", e)
         return null;
     }
 }
