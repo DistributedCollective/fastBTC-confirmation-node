a9f628090bde7ecdf82651cf56de633323d2fa25
Antti Haapala <antti.haapala@anttipatterns.com>
2022-03-16 13:17:19 +0200

    bsc changes + stricter tx checking to mainnet

--- config/config_mainnet.js
+++ config/config_mainnet.js
@@ -8,7 +8,11 @@ export default {
     btcNodeProvider: {},
     network: "main",
     db: "node_mainnet",
-    multisigAddress: "0x0f279e810B95E0d425622b9b40D7bCD0B5C4B19d",
+    contractAddress: '0xC9e14126E5796e999890a4344b8e4c99Ac7002A1'.toLowerCase(),
+    bscBridgeAddress: '0x1CcAd820B6d031B41C54f1F3dA11c0d48b399581'.toLowerCase(), // RSK network RSK-BSC bridge. TODO: double check
+    bscAggregatorAddress: '0xF300e09958cEE25453da4D7405045c43bFec602f'.toLowerCase(),  // BSC network BTCs aggregator. TODO: double check
+    multisigAddress: "0x0f279e810B95E0d425622b9b40D7bCD0B5C4B19d".toLowerCase(),
+    bscPrefix: 'bsc:',
     walletSigs,
     startIndex: 12000, //tx-index
     account: accounts["main"],
