1393951db2becf72b751c3df57d65ee7fb49723a
Antti Haapala <antti.haapala@anttipatterns.com>
2022-06-16 15:59:16 +0300

    Re-enable BSC bridge with new addresses. Harmonize quotes in config.

--- config/config_mainnet.js
+++ config/config_mainnet.js
@@ -1,22 +1,22 @@
 import telegramBot from '../secrets/telegram.js';
 import walletSigs from '../secrets/walletSigs.main';
-import accounts from "../secrets/accounts";
+import accounts from '../secrets/accounts';
 
 export default {
     masterNode: 'http://3.131.33.161:3000/',
     rskNodeProvider: 'https://mainnet.sovryn.app/rpc',
     btcNodeProvider: {},
-    network: "main",
-    db: "node_mainnet",
+    network: 'main',
+    db: 'node_mainnet',
     contractAddress: '0xC9e14126E5796e999890a4344b8e4c99Ac7002A1'.toLowerCase(),
-    bscBridgeAddress: '0x1CcAd820B6d031B41C54f1F3dA11c0d48b399581'.toLowerCase(), // RSK network RSK-BSC bridge. TODO: double check
-    bscAggregatorAddress: '0xF300e09958cEE25453da4D7405045c43bFec602f'.toLowerCase(),  // BSC network BTCs aggregator. TODO: double check
-    multisigAddress: "0x0f279e810B95E0d425622b9b40D7bCD0B5C4B19d".toLowerCase(),
+    multisigAddress: '0x0f279e810B95E0d425622b9b40D7bCD0B5C4B19d'.toLowerCase(),
+    bscBridgeAddress: '0x971b97c8cc82e7d27bc467c2dc3f219c6ee2e350'.toLowerCase(),
+    bscAggregatorAddress: '0x1dA3D286a3aBeaDb2b7677c99730D725aF58e39D'.toLowerCase(),  // BSC network BTCs aggregator.
     bscPrefix: 'bsc:',
     walletSigs,
     startIndex: 12000, //tx-index
-    account: accounts["main"],
-    blockExplorer: "https://explorer.rsk.co",
-    sovrynInternalTelegramId: -1001469142339, 
+    account: accounts['main'],
+    blockExplorer: 'https://explorer.rsk.co',
+    sovrynInternalTelegramId: -1001469142339,
     errorBotTelegram: Object.keys(telegramBot).length > 0 ? telegramBot : null
 }
